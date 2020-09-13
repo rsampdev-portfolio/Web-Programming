d01b9991061bf99e380031c62eaa6fea0f9f60d0
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-12 22:12:06 -0400

    Creating random test transactions

--- projects/project-one.js
+++ projects/project-one.js
@@ -1,6 +1,20 @@
 class Transaction {
-    constructor(date, ammount) {
+    constructor(date, amount) {
         this.date = date;
-        this.ammount = ammount;
+        this.amount = amount;
     }
 }
+
+let transactions = [];
+
+for (let i = 0; i < 100; i++) {
+    let dollar = Math.random() * 100;
+    let cents = Math.random();
+    let amount = (dollar + cents).toFixed(2);
+
+    let transaction = new Transaction("Test", amount);
+    
+    transactions.push(transaction);
+}
+
+console.log(transactions);

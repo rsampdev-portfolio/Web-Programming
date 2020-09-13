3024e8f4758dfb9d50b32f548195a2e74916bcfe
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-12 23:08:26 -0400

    Use closures to build random transactions

--- projects/project-one.js
+++ projects/project-one.js
@@ -5,16 +5,32 @@ class Transaction {
     }
 }
 
-let transactions = [];
-
-for (let i = 0; i < 100; i++) {
-    let dollar = Math.random() * 100;
-    let cents = Math.random();
-    let amount = (dollar + cents).toFixed(2);
-
-    let transaction = new Transaction("Test", amount);
+function TransactionBuilder() {
+    function addDays(date, days) {
+        const copy = new Date(Number(date));
+        copy.setDate(date.getDate() + days);
+        return copy;
+    }
     
-    transactions.push(transaction);
+    let date = new Date(new Date().getFullYear() + 1, 0, 0, 0, 0, 0, 0);
+
+    return function () {
+        let amount = parseFloat((Math.random() * 100 + Math.random()).toFixed(2));
+        let transaction = new Transaction(date, amount);
+        date = addDays(date, 1);
+        return transaction;
+    }
 }
 
+let transactionBuilder = TransactionBuilder();
+
+let transactions = [];
+
+for (let i = 0; i < 365; i++) {
+    transactions.push(transactionBuilder());
+}
+
+let totalAmountSum = transactions.reduce((accumulator, transaction) => accumulator + transaction.amount, 0).toFixed(2);
+
 console.log(transactions);
+console.log(totalAmountSum);

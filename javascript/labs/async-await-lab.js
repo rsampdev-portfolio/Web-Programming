0ecde40e7f78cc754a0c0ae86a64cd39549f3850
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 01:27:51 -0400

    Complete Async Await Lab

--- javascript/labs/async-await-lab.js
+++ javascript/labs/async-await-lab.js
@@ -8,18 +8,12 @@ const getX = () => getNumber(3);
 const getY = () => getNumber(5);
 const getZ = () => getNumber(2);
 
-// TODO: Make the following function `async` and replace the nested `.then` calls with
-//      `await` statements that assign to local variables.
-//      The result should still be the same, logging out the sum of `10`
-// HINT: With the `await` statements, there is no need for any nesting.
-function sumData() {
-    return getX().then(x => {
-        return getY().then(y => {
-            return getZ().then(z => {
-                console.log(x + y + z);
-            });
-        });
-    });
+async function sumData() {
+    let x = await getX();
+    let y = await getY();
+    let z = await getZ();
+
+    console.log(x + y + z);
 }
 
 sumData();

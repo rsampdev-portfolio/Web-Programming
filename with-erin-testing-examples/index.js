2f4226d25267aeb4ad7cb8a0fcdcf58379551962
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 13:57:42 -0400

    Add calculator module

--- with-erin-testing-examples/index.js
+++ with-erin-testing-examples/index.js
@@ -1,1 +1,9 @@
+import calc from "../modules/calculator.js"
+
+let sum = calc.add(2, 4);
+console.log(sum);
+
+let product = calc.multiply(3, 4);
+console.log(product);
+
 console.log("Hello, World!");

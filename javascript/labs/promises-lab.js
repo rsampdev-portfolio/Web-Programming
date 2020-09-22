627a1394e59ecca0a66a2e815e32e82fd4c2479f
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 01:03:50 -0400

    Complete Promises Lab

--- javascript/labs/promises-lab.js
+++ javascript/labs/promises-lab.js
@@ -1,9 +1,11 @@
-function getData(callback) {
+function getData() {
     const data = [1, 2, 3];
-    setTimeout(() => callback(data), 1000);
+
+    return new Promise(resolve => {
+        const sum = data.reduce((a, b) => a + b, 0);
+        console.log(sum);
+        resolve(sum);
+    });
 }
 
-getData(function (data) {
-    const sum = data.reduce((a, b) => a + b, 0);
-    console.log(sum);
-});
+getData().then();

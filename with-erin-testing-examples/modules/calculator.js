a202dbe28e70d972af079bf3f0cbba1bbbb71144
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 16:56:42 -0400

    Refactor calculator.js module

--- with-erin-testing-examples/modules/calculator.js
+++ with-erin-testing-examples/modules/calculator.js
@@ -1,9 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
+let add = (a, b) => a + b;
 
-function multiply(n1, n2) {
-    return n1 * n2;
-}
+let subtract = (a, b) => a - b;
 
-export default { add, multiply }
+let multiply = (a, b) => a * b;
+
+let divide = (a, b) => a / b;
+
+export default { add, subtract, multiply, divide };

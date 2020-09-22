2fe19618fa2ab7089ddc0de1b5ea678dc0e79975
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 17:09:19 -0400

    Complete add tests

--- with-erin-testing-examples/modules/calculator.test.js
+++ with-erin-testing-examples/modules/calculator.test.js
@@ -2,15 +2,30 @@ import calc from "./calculator.js"
 
 describe("#add", () => {
     describe("Given two numbers are passed", () => {
-        describe("When the numbers are 2 and 100", () => {
-            it("Should return 102", () => {
-                expect(calc.add(2, 100)).toBe(102);
+        describe("When both numbers are positive: 10 and 10", () => {
+            it("Should return 20", () => {
+                expect(calc.add(10, 10)).toBe(20);
             });
         });
-        describe("When the number are -1 and 50", () => {
-            it("Should return 49", () => {
-                expect(calc.add(-1, 50)).toBe(49);
+        describe("When both numbers are negative: -10 and -10", () => {
+            it("Should return -20", () => {
+                expect(calc.add(-10, -10)).toBe(-20);
             });
-        })
+        });
+        describe("When one number is positive and the other is negative: 10 and -10", () => {
+            it("Should return 0", () => {
+                expect(calc.add(10, -10)).toBe(0);
+            });
+        });
+        describe("When one number is 0 and the other is not: 0 and 10", () => {
+            it("Should return 10", () => {
+                expect(calc.add(0, 10)).toBe(10);
+            });
+        });
+        describe("When both numbers are zero: 0 and 0", () => {
+            it("Should return 0", () => {
+                expect(calc.add(0, 0)).toBe(0);
+            });
+        });
     });
 });

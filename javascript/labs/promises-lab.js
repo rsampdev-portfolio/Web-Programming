7065396afcc90790437a79e54e1eb4ae902bbb0e
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-22 01:41:51 -0400

    Add setTimeout() back to the promise

--- javascript/labs/promises-lab.js
+++ javascript/labs/promises-lab.js
@@ -2,9 +2,11 @@ function getData() {
     const data = [1, 2, 3];
 
     return new Promise(resolve => {
-        const sum = data.reduce((a, b) => a + b, 0);
-        console.log(sum);
-        resolve(sum);
+        setTimeout(() => {
+            const sum = data.reduce((a, b) => a + b, 0);
+            console.log(sum);
+            resolve(sum)
+        }, 1000);
     });
 }
 

7614cfd0af992f42d81837f4dec9baafed74aabf
Rodney S. Sampson II <rsampdev@gmail.com>
2020-09-28 01:26:25 -0400

    Remove unneeded functions

--- with-erin-testing-examples/modules/calculator.js
+++ with-erin-testing-examples/modules/calculator.js
@@ -2,8 +2,4 @@ let add = (a, b) => a + b;
 
 let subtract = (a, b) => a - b;
 
-let multiply = (a, b) => a * b;
-
-let divide = (a, b) => a / b;
-
-export default { add, subtract, multiply, divide };
+export default { add, subtract };

143f7c62cc045036365d0a88c931deb86e103ecd
Rodney S. Sampson II <rsampdev@gmail.com>
2020-10-20 11:21:16 -0400

    Complete lab

--- react/React-JSX-Expressions-Lab/src/App.js
+++ react/React-JSX-Expressions-Lab/src/App.js
@@ -8,47 +8,61 @@ function formatPrice(price) {
 }
 
 class App extends Component {
+    renderShoppingCartHeaders() {
+        const headers = ["Item", "Description", "Quantity", "Price", "Total"];
 
-    render() {
-        const { cart } = this.props;
+        return (
+            <div className={styles.product + ' ' + styles.header}>
+                {headers.map((headerTitle) => <h3>{headerTitle}</h3>)}
+            </div>
+        );
+    }
+    
+    renderShoppingCartItems(cart) {
+        return (
+            cart.map(item => (
+                <div key={item.id} className={styles.product}>
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                    <p>{item.quantity}</p>
+                    <p>{formatPrice(item.price)}</p>
+                    <p>{formatPrice(item.quantity * item.price)}</p>
+                </div>
+            ))
+        );
+    }
 
+    renderShoppingCartItemsTotal(cart) {
+        return (
+            <div className={styles.product + ' ' + styles.header}>
+                <p></p>
+                <p></p>
+                <p></p>
+                <h3>Total</h3>
+                <h3 style={{ color: "black"}}>
+                    {formatPrice(cart.reduce((total, item) => total + item.quantity * item.price, 0))}
+                </h3>
+            </div>
+        );
+    }
+
+    renderShoppingCart(cart) {
         return (
             <section className={styles.App}>
                 <h1>Shopping Cart</h1>
                 <div className={styles['product-list']}>
-                    <>
-                        <div className={styles.product + ' ' + styles.header}>
-                            <h3>Item</h3>
-                            <h3>Description</h3>
-                            <h3>Quantity</h3>
-                            <h3>Price</h3>
-                            <h3>Total</h3>
-                        </div>
-                        {
-                            cart.map(item => (
-                                <div key={item.id} className={styles.product}>
-                                    <p>{item.name}</p>
-                                    <p>{item.description}</p>
-                                    <p>{item.quantity}</p>
-                                    <p>{formatPrice(item.price)}</p>
-                                    <p>{formatPrice(item.quantity * item.price)}</p>
-                                </div>
-                            ))
-                        }
-                        <hr />
-                        <div className={styles.product + ' ' + styles.header}>
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <h3>Total</h3>
-                            <h3 style={{ color: "black"}}>
-                                {formatPrice(cart.reduce((total, item) => total + item.quantity * item.price, 0))}
-                            </h3>
-                        </div>
-                    </>
+                    {this.renderShoppingCartHeaders()}
+                    {this.renderShoppingCartItems(cart)}
+                    <hr />
+                    {this.renderShoppingCartItemsTotal(cart)}
                 </div>
             </section>
         );
+    }
+
+    render() {
+        const cart = this.props.cart;
+        return this.renderShoppingCart(cart);
     }
 }
 

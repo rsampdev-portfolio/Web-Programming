679aca51ab3a59fdd252bf978cf21d18f6153a41
Rodney S. Sampson II <rsampdev@gmail.com>
2020-11-12 09:37:22 -0500

    Style header and update content centering

--- react/project-three/src/App.js
+++ react/project-three/src/App.js
@@ -38,6 +38,12 @@ class App extends Component {
     }
   }
 
+  linker(url, text) {
+    return (
+      <Link to={`/${url}`}><b><a>{`${text}`}</a></b></Link>
+    );
+  }
+
   render() {
     return (
       <>
@@ -46,77 +52,79 @@ class App extends Component {
             <span>
               <nav>
                 <ul>
-                  <li><Link to="/all">All Months</Link></li>
+                  <li>{this.linker("january", "January")}</li>
+                  <li>{this.linker("february", "February")}</li>
+                  <li>{this.linker("march", "March")}</li>
+                  <li>{this.linker("april", "April")}</li>
+                  <li>{this.linker("may", "May")}</li>
+                  <li>{this.linker("june", "June")}</li>
+                  <li>{this.linker("july", "July")}</li>
                 </ul>
                 <ul>
-                  <li><Link to="/january">January</Link></li>
-                  <li><Link to="/february">February</Link></li>
-                  <li><Link to="/march">March</Link></li>
-                  <li><Link to="/april">April</Link></li>
-                  <li><Link to="/may">May</Link></li>
-                  <li><Link to="/june">June</Link></li>
-                  <li><Link to="/july">July</Link></li>
-                  <li><Link to="/august">August</Link></li>
-                  <li><Link to="/september">September</Link></li>
-                  <li><Link to="/october">October</Link></li>
-                  <li><Link to="/november">November</Link></li>
-                  <li><Link to="/december">December</Link></li>
+                  <li>{this.linker("august", "August")}</li>
+                  <li>{this.linker("september", "September")}</li>
+                  <li>{this.linker("october", "October")}</li>
+                  <li>{this.linker("november", "November")}</li>
+                  <li>{this.linker("december", "December")}</li>
+                  <li>{this.linker("all", "All Months")}</li>
                 </ul>
               </nav>
             </span>
 
-            <Switch>
-              <Route exact path={["/", "/all"]}>
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[0] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[1] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[2] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[3] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[4] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[5] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[6] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[7] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[8] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[9] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[10] : null} />
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[11] : null} />
-              </Route>
-              <Route exact path="/january">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[0] : null} />
-              </Route>
-              <Route exact path="/february">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[1] : null} />
-              </Route>
-              <Route exact path="/march">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[2] : null} />
-              </Route>
-              <Route exact path="/april">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[3] : null} />
-              </Route>
-              <Route exact path="/may">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[4] : null} />
-              </Route>
-              <Route exact path="/june">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[5] : null} />
-              </Route>
-              <Route exact path="/july">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[6] : null} />
-              </Route>
-              <Route exact path="/august">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[7] : null} />
-              </Route>
-              <Route exact path="/september">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[8] : null} />
-              </Route>
-              <Route exact path="/october">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[9] : null} />
-              </Route>
-              <Route exact path="/november">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[10] : null} />
-              </Route>
-              <Route exact path="/december">
-                <MonthlyReportContainer month={this.state.data != null ? this.state.data[11] : null} />
-              </Route>
-            </Switch>
+            <div id="Content-Container">
+              <Switch>
+                <Route exact path={["/", "/all"]}>
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[0] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[1] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[2] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[3] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[4] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[5] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[6] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[7] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[8] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[9] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[10] : null} />
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[11] : null} />
+                </Route>
+                <Route exact path="/january">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[0] : null} />
+                </Route>
+                <Route exact path="/february">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[1] : null} />
+                </Route>
+                <Route exact path="/march">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[2] : null} />
+                </Route>
+                <Route exact path="/april">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[3] : null} />
+                </Route>
+                <Route exact path="/may">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[4] : null} />
+                </Route>
+                <Route exact path="/june">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[5] : null} />
+                </Route>
+                <Route exact path="/july">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[6] : null} />
+                </Route>
+                <Route exact path="/august">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[7] : null} />
+                </Route>
+                <Route exact path="/september">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[8] : null} />
+                </Route>
+                <Route exact path="/october">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[9] : null} />
+                </Route>
+                <Route exact path="/november">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[10] : null} />
+                </Route>
+                <Route exact path="/december">
+                  <MonthlyReportContainer month={this.state.data != null ? this.state.data[11] : null} />
+                </Route>
+              </Switch>
+            </div>
           </Router>
         </div>
       </>

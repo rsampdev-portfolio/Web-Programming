2ab2258246302b9c71f0af2c3c154c795167629c
Rodney S. Sampson II <rsampdev@gmail.com>
2020-11-12 10:24:23 -0500

    Streamline MonthlyReportContainer component

--- react/project-three/src/components/monthly-report-container/monthly-report-container.js
+++ react/project-three/src/components/monthly-report-container/monthly-report-container.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
-class MonthlyReportContainer extends Component {
-    helper(month, number) {
-        return `${month.transactionReasonPercentages[number].reason} - ${month.transactionReasonPercentages[number].percentage}%`
+import "../monthly-report-container/monthly-report-container.css";
+
+function MonthlyReportContainer(props) {
+    function helper(month, number) {
+        return `${month.transactionReasonPercentages[number].reason} `
+        + `- ${month.transactionReasonPercentages[number].percentage}%`
         + ` - $${(month.transactionAmountSum * month.transactionReasonPercentages[number].percentage).toFixed(2)}`
     }
-    render() {
-        let month = this.props.month;
 
-        if (month === null || month === undefined) {
-            return null;
-        }
+    let month = props.month;
 
-        month.transactionReasonPercentages.sort((a, b) => b.percentage - a.percentage);
-        
-        return (
-            <div>
-                <h3>{month.month}</h3>
-
-                <h4>{`Total Monthly Transactions - $${month.transactionAmountSum}`}</h4>
-
-                <h4>{"Transaction Reason Percentages:"}</h4>
-
-                <p>{this.helper(month, 0)}</p>
-
-                <p>{this.helper(month, 1)}</p>
-
-                <p>{this.helper(month, 2)}</p>
-
-                <p>{this.helper(month, 3)}</p>
-
-                <p>{this.helper(month, 4)}</p>
-            </div>
-        );
+    if (month === null || month === undefined) {
+        return null;
     }
+
+    month.transactionReasonPercentages.sort((a, b) => b.percentage - a.percentage);
+
+    return (
+        <div>
+            <h3 id="Month-Header">{month.month}</h3>
+
+            <h4>{`Total Monthly Transactions: $${month.transactionAmountSum}`}</h4>
+
+            <h4>{"Transaction Reason Percentages:"}</h4>
+
+            <p>{helper(month, 0)}</p>
+
+            <p>{helper(month, 1)}</p>
+
+            <p>{helper(month, 2)}</p>
+
+            <p>{helper(month, 3)}</p>
+
+            <p>{helper(month, 4)}</p>
+        </div>
+    );
 }
 
 export default MonthlyReportContainer;
